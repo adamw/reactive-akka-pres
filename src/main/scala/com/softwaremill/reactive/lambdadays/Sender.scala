37373bfae87a9d78a5025efbbde042752cd521b4
adamw <adam@warski.org>
2015-02-25 12:47:18 +0100

    Slides

---
+++ src/main/scala/com/softwaremill/reactive/lambdadays/Sender.scala
@@ -0,0 +1,27 @@
+package com.softwaremill.reactive.lambdadays
+
+import java.net.InetSocketAddress
+
+import akka.actor.ActorSystem
+import akka.stream.ActorFlowMaterializer
+import akka.stream.scaladsl._
+import akka.util.ByteString
+import com.softwaremill.reactive._
+
+object Sender extends App with Logging {
+  implicit val system = ActorSystem()
+  val serverConnection = StreamTcp().outgoingConnection(new InetSocketAddress("localhost", 9182))
+
+  val getLines = () => scala.io.Source.fromFile("/Users/adamw/projects/reactive-akka-pres/data/2008.csv").getLines()
+
+  val linesSource = Source(getLines).map { line => ByteString(line + "\n") }
+  val logCompleteSink = Sink.onComplete(r => logger.info("Completed with: " + r))
+
+  val flow = linesSource
+    .via(serverConnection.flow)
+    .to(logCompleteSink)
+
+  implicit val mat = ActorFlowMaterializer()
+  flow.run()
+}
+
